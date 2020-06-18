a6eda470f577855986900a96e1beb21facf04de4
Noel <noelalonso1986@gmail.com>
2020-06-18 08:44:29 +0100

    Añade nuevo perfil de test

--- src/test/java/es/redmic/oauth/ApiOauthApplicationTests.java
+++ src/test/java/es/redmic/oauth/ApiOauthApplicationTests.java
@@ -1,5 +1,7 @@
 package es.redmic.oauth;
 
+import static org.junit.Assert.assertTrue;
+
 /*-
  * #%L
  * OAuth
@@ -9,9 +11,9 @@ package es.redmic.oauth;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,11 +32,12 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 public class ApiOauthApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertTrue(true);
 	}
 
 }
